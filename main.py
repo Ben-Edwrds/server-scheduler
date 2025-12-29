ac1f12a1ac35458aca9ff3eee4cfb37cb122769d
Ben-Edwrds <ben.edwards@student.vives.be>
2025-12-29 18:39:50 +0100

    added datetime validation

--- main.py
+++ main.py
@@ -7,14 +7,26 @@ Created on Mon Dec 29 16:57:13 2025
 
 from db import get_connection, init_db, add_schedule, list_schedules
 from settings import DB_PATH
+from datetime import datetime
 
 conn = get_connection(DB_PATH)
 init_db(conn)
 
+def check_datetime(prompt: str) -> str:
+    while True:
+        value = input(prompt)
+        try:
+            dt = datetime.strptime(value, "%Y-%m-%d %H:%M")
+            return dt.strftime("%Y-%m-%d %H:%M")
+        except ValueError:
+            print("Use format: YYYY-MM-DD HH:MM")
+
+
 name = input("Name: ")
-sleep_time = input("Sleep time (YYYY-MM-DD HH:MM): ")
-wake_time = input("Wake time (YYYY-MM-DD HH:MM): ")
+sleep_time = check_datetime("Sleep time: YYYY-MM-DD HH:MM")
+wake_time = check_datetime("Wake time: YYYY-MM-DD HH:MM")
 mode = input("Mode (mem/disk/off): ")
+
 new_id = add_schedule(conn, name, sleep_time, wake_time, mode, enabled=1)
 
 print("Schedule added with ID: " + str(new_id))
