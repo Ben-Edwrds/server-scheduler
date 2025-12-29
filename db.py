ee459a6c45d1f095bae586539c0466d5df26ec46
Ben-Edwrds <ben.edwards@student.vives.be>
2025-12-29 17:15:31 +0100

    initialise db, connect main and test

--- db.py
+++ db.py
@@ -7,29 +7,32 @@ Created on Mon Dec 29 15:46:30 2025
 
 import sqlite3
 
-connection = sqlite3.connect("chinook.db")
-cursor = connection.cursor()
+def get_connection(db_path: str) -> sqlite3.Connection:
+    return sqlite3.connect(db_path)
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS schedules (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    sleep_time TEXT NOT NULL,
-    wake_time TEXT NOT NULL,
-    mode TEXT NOT NULL,
-    enabled INTEGER NOT NULL DEFAULT 1
-);
-""")
+def init_db(connection: sqlite3.Connection) -> None:
+    cursor = connection.cursor()
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS runs (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    schedule_id INTEGER NOT NULL,
-    action TEXT NOT NULL,
-    executed_at TEXT NOT NULL,
-    status TEXT NOT NULL,
-    FOREIGN KEY (schedule_id) REFERENCES schedules(id)
-);
-""")
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS schedules (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        sleep_time TEXT NOT NULL,
+        wake_time TEXT NOT NULL,
+        mode TEXT NOT NULL,
+        enabled INTEGER NOT NULL DEFAULT 1
+    );
+    """)
 
-connection.commit()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS runs (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        schedule_id INTEGER NOT NULL,
+        action TEXT NOT NULL,
+        executed_at TEXT NOT NULL,
+        status TEXT NOT NULL,
+        FOREIGN KEY (schedule_id) REFERENCES schedules(id)
+    );
+    """)
+
+    connection.commit()
--- main.py
+++ main.py
@@ -5,3 +5,8 @@ Created on Mon Dec 29 16:57:13 2025
 @author: bened
 """
 
+from db import get_connection, init_db
+conn = get_connection("project.db")
+init_db(conn)
+
+print("blablatest")
