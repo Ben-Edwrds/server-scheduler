fe7a98413c4b322da39c2fdff91f2c0037d66f6e
Ben-Edwrds <ben.edwards@student.vives.be>
2025-12-29 17:22:18 +0100

    fill db with info

--- db.py
+++ db.py
@@ -7,7 +7,7 @@ Created on Mon Dec 29 15:46:30 2025
 
 import sqlite3
 
-def get_connection(db_path: str) -> sqlite3.Connection:
+def get_connection(str(db_path)) -> sqlite3.Connection:
     return sqlite3.connect(db_path)
 
 def init_db(connection: sqlite3.Connection) -> None:
@@ -34,5 +34,31 @@ def init_db(connection: sqlite3.Connection) -> None:
         FOREIGN KEY (schedule_id) REFERENCES schedules(id)
     );
     """)
+    
+    def add_schedule(connection, str(name), str(sleep_time), str(wake_time), str(mode), int(enabled) = 1):
+        cursor = connection.cursor()
+        cursor.execute("""
+        INSERT INTO schedules (name, sleep_time, wake_time, mode, enabled)
+        VALUES (?, ?, ?, ?, ?)
+    """, (name, sleep_time, wake_time, mode, enabled))
+    connection.commit()
+    return cursor.lastrowid
+
+    def list_schedules(connection, bool(enabled) = false):
+        cursor = connection.cursor()
+        if enabled:
+            cursor.execute("""
+           SELECT id, name, sleep_time, wake_time, mode, enabled
+           FROM schedules
+           WHERE enabled = 1
+           ORDER BY wake_time
+       """)
+   else:
+       cursor.execute("""
+           SELECT id, name, sleep_time, wake_time, mode, enabled
+           FROM schedules
+           ORDER BY wake_time
+       """)
+       return cursor.fetchall()
 
     connection.commit()
